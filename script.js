42fe3c649151e20420899d37b6f97f6baf9c6c6d
ocos <1710831+ocos@users.noreply.github.com>
2018-04-02 14:31:48 +0200

    fixed focus on search input moved hardcoded shortcuts strings in shortcuts object

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // note: key library was modified to enable capture in addEventListener
 
 (function() {
+
+    var shortcuts = {
+        openInNewTab : '⌘+return, ctrl+return',
+        open : 'return',
+        up : 'k, up',
+        down : 'j, down',
+        focusInSearchInput : '/, backspace',
+    };
+
+    var googleHtmlIdentifiers = {
+        searchInput : '#lst-ib'
+    };
+
     var idx = 0;
     var select = function(focus) {
         $('h3.r a').css('background-color', 'inherit');
@@ -9,13 +22,13 @@
         if (focus) {link.focus(); }
         return link;
     };
-    key('⌘+return', function(ev) {
+    key(shortcuts.openInNewTab, function(ev) {
         var link = select();
         window.open(link.attr('href'));
         ev.stopPropagation();
         ev.preventDefault();
     });
-    key('return', function(ev) {
+    key(shortcuts.open, function(ev) {
         var link = select();
         location.href = link.attr('href');
         ev.stopPropagation();
@@ -35,22 +48,29 @@
         });
     });
 
-    key('j', function(ev) {
+    key(shortcuts.down, function(ev) {
         if (idx < $('h3.r a').length-1) {
             idx++;
             select(true);
         }
         ev.stopPropagation();
     });
-    key('k', function(ev) {
+    key(shortcuts.up, function(ev) {
         if (idx > 0) {
             idx--;
             select(true);
         }
         ev.stopPropagation();
     });
-    key('/', function(ev) {
-        $('#gbqfq').focus();
+    key(shortcuts.focusInSearchInput, function (ev) {
+        var searchInput = $(googleHtmlIdentifiers.searchInput);
+        var textInitial = searchInput.val();
+
+        searchInput
+             .val('')
+             .val(textInitial)
+             .focus();
+
         ev.stopPropagation();
         ev.preventDefault();
     });
